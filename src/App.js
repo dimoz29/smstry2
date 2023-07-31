fd2f532e11707e485c1a30e9360eca663470950f
dimoz29 <123158855+dimoz29@users.noreply.github.com>
2023-07-31 18:21:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="media/logo.f7879e226ed5822062e2.png" alt="Header" className="App-logo" />
+        <img src="https://imgur.com/a/dWfm3N8" alt="Header" className="App-logo" />
         <h1>Payment dApp</h1>
         {accounts.length === 0 ? (
           <button onClick={initializeWeb3}>Connect Wallet</button>
