2df298d4155b566f537d4b1480a2bc451dab618c
Ricardo Jorge Almeida <113598600+ricardojalmeida@users.noreply.github.com>
2023-08-01 12:12:04 +0300

    Update App.js
    Correct Smart Contract input

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
     }
 
     try {
-      await contract.methods.sendPayment(message).send({
+      await contract.methods.sendPayment().send({
         from: accounts[0],
         value: web3.utils.toWei('0.2', 'ether'),
       });
