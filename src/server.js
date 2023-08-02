d98a48a0569b7496d73714ab01f158f1594342fa
Georgios Papadopoulos <g.papadopoulos@eu.telekom.com>
2023-08-02 12:07:29 +0300

    testin

--- src/server.js
+++ src/server.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const vonage = require('@vonage/server-sdk');
-
-const vonage = new Vonage({
-  apiKey: "473d3aba",
-  apiSecret: "ZIu8VnYsav99tSew"
-});
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/send-sms', (req, res) => {
+app.post('/send-sms', async (req, res) => {
   const { to, text } = req.body;
-  vonage.sms.send(to, "WEB3SMS", text, (err, responseData) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send({ message: 'There was an error sending the messages.' });
-    } else {
-      console.log(responseData);
-      res.status(200).send({ message: 'Message sent successfully' });
-    }
-  });
+
+  const data = {
+    api_key: 'YOUR_API_KEY',
+    api_secret: 'YOUR_API_SECRET',
+    to: to,
+    from: 'WEB3SMS',
+    text: text,
+  };
+
+  try {
+    const response = await axios.post('https://rest.nexmo.com/sms/json', data);
+    console.log(response.data);
+    res.status(200).send({ message: 'Message sent successfully' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: 'There was an error sending the message.' });
+  }
 });
 
 app.listen(4000, () => console.log('Server is running on port 4000'));
