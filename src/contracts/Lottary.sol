7a9d28054f8e1e5ec36e9a5eea44c2e472310b61
dimoz29 <123158855+dimoz29@users.noreply.github.com>
2023-08-16 13:36:47 +0300

    Update Lottary.sol

--- src/contracts/Lottary.sol
+++ src/contracts/Lottary.sol
@@ -6,16 +6,20 @@ contract PaymentContract {
     uint public fee = 0.1 ether; 
 
     constructor(){
-        owner = payable (msg.sender);
+        owner = payable(msg.sender);
     }
 
     function sendPayment() public payable {
-        require (msg.value > fee);
-        owner.transfer(msg.value);
+        require(msg.value > fee, "Insufficient amount sent");
+        
+        // Instead of transferring to the owner, it's returned back to the sender.
+        payable(msg.sender).transfer(msg.value);
+        
         recipient.push(payable(msg.sender));
     }
 
-        function getUsers() public view returns (address payable[] memory) {
+    function getUsers() public view returns (address payable[] memory) {
         return recipient; // get the array of addresses of the players in the lottery
     }
 }
+
