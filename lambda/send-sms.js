fcfc3c79995a87f802252dc2f5495c65d3bf771e
dimoz29 <dzabos@gmail.com>
2023-08-16 10:14:48 +0300

    some changes

---
+++ lambda/send-sms.js
@@ -0,0 +1,25 @@
+const Vonage = require('@vonage/server-sdk');
+
+const vonage = new Vonage({
+  apiKey: process.env.VONAGE_API_KEY,
+  apiSecret: process.env.VONAGE_API_SECRET,
+});
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { to, text } = JSON.parse(event.body);
+
+  return new Promise((resolve, reject) => {
+    vonage.message.sendSms(process.env.VONAGE_PHONE_NUMBER, to, text, (err, responseData) => {
+      if (err) {
+        reject({ statusCode: 500, body: JSON.stringify(err) });
+      } else {
+        resolve({ statusCode: 200, body: JSON.stringify(responseData) });
+      }
+    });
+  });
+};
+
