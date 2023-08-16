28f9b3b96e4e1eaf5f5204a9a9635174f0a22fcf
dimoz29 <123158855+dimoz29@users.noreply.github.com>
2023-08-16 12:23:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
     try {
       await contract.methods.sendPayment().send({
         from: accounts[0],
-        value: web3.utils.toWei('0.07', 'ether'),
+        value: web3.utils.toWei('0.2', 'ether'),
       });
 
       // Send SMS
