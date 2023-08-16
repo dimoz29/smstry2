ec0cca2216284b6871a6a745cab512d0dbfb9727
dimoz29 <123158855+dimoz29@users.noreply.github.com>
2023-08-16 13:17:40 +0300

    Update send-sms.js

--- lambda/send-sms.js
+++ lambda/send-sms.js
@@ -3,6 +3,7 @@ const Vonage = require('@vonage/server-sdk');
 const vonage = new Vonage({
   apiKey: process.env.VONAGE_API_KEY,
   apiSecret: process.env.VONAGE_API_SECRET,
+  applicationId: process.env.VONAGE_APPLICATION_ID
 });
 
 
