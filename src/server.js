056c0cf1db3ce2c6ea491bafec0a67525ccf1811
dimoz29 <123158855+dimoz29@users.noreply.github.com>
2023-09-20 19:40:31 +0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const vonage = require('@vonage/server-sdk');
+
+const vonage = new Vonage({
+  apiKey: "473d3aba",
+  apiSecret: "ZIu8VnYsav99tSew"
+});
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/send-sms', (req, res) => {
+  const { to, text } = req.body;
+  vonage.sms.send(to, "WEB3SMS", text, (err, responseData) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({ message: 'There was an error sending the messages.' });
+    } else {
+      console.log(responseData);
+      res.status(200).send({ message: 'Message sent successfully' });
+    }
+  });
+});
+
+app.listen(4000, () => console.log('Server is running on port 4000'));
